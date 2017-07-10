9520d50c4f371fc0d4e1794bfc644893b3fa42de
BFAlex <838318741@qq.com>
2017-07-10 10:15:58 +0800

    Initial Commit

---
+++ BFHUDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFHUDDemo
+//
+//  Created by Readboy_BFAlex on 2017/7/10.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFHUDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFHUDDemo
+//
+//  Created by Readboy_BFAlex on 2017/7/10.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
